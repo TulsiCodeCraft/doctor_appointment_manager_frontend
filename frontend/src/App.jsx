69b536f38ee7454c14742c82cdc179062ede4ee0
TulsiCodeCraft <152911625+TulsiCodeCraft@users.noreply.github.com>
2024-12-28 12:58:09 +0530

    render url added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   useEffect(()=>{
     const fetchUser=async()=>{
       try{
-        const response=await axios.get("http://localhost:4000/api/v1/user/patient/me",{
+        const response=await axios.get("https://appointment-backend-zci4.onrender.com/api/v1/user/patient/me",{
           withCredentials:true
         });
         setIsAuthenticated(true);
